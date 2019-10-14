a4904a65dcd034a31c8a598c5967fd952af94dc2
Sai Teja Suram <pratap130492@gmail.com>
2019-10-13 20:20:28 -0700

    Import bash history

--- recent2.py
+++ recent2.py
@@ -23,8 +23,10 @@ class Term:
 
 class SQL:
 
-    INSERT_ROW = """insert into commands (command_dt, command, pid, return_val, pwd, session)
-        values (datetime('now','localtime'), ?, ?, ?, ?, ?)"""
+    INSERT_ROW_CUSTOM_BASE = """insert into commands (command_dt, command, pid, return_val, pwd, session)
+        values ({}, ?, ?, ?, ?, ?)"""
+    INSERT_ROW = INSERT_ROW_CUSTOM_BASE.format("datetime('now', 'localtime')")
+    INSERT_ROW_CUSTOM_TS = INSERT_ROW_CUSTOM_BASE.format("datetime(?, 'unixepoch')")
     INSERT_SESSION = """insert into sessions (created_dt, updated_dt,
         term, hostname, user, sequence, session)
         values (datetime('now','localtime'), datetime('now','localtime'), ?, ?, ?, ?, ?)"""
@@ -142,6 +144,7 @@ def build_schema(conn):
         migrate(0, conn)
 
 
+# Entry point to recent-log command.
 def log():
     parser = argparse.ArgumentParser()
     parser.add_argument('-r', '--return_value', help='set to $?', default=0)
@@ -172,6 +175,74 @@ def log():
         c.execute(SQL.INSERT_ROW, [command, pid,
                                    return_value, pwd, session.id])
 
+    conn.commit()
+    conn.close()
+
+
+# Imports bash_history into RECENT_DB
+# Entry point to recent-import-bash-history command.
+def import_bash_history():
+    import sys
+    if len(sys.argv) != 1:
+        # Help and stuff (Quick and dirty)
+        exit_status = 0
+        if set(sys.argv[1:]) not in ({'-h'}, {'--help'}):
+            print(sys.argv[0] + ": unknown args passed")
+            exit_status = 1
+        print(sys.argv[0] + ": Imports bash history into ~/.recent.db")
+        exit(exit_status)
+    # Construct history from bash_history.
+    # Example bash_history. The history has 3 entries. First entry has no timestamp attached to it.
+    # The next 2 entries have timestamp attached to them. The last entry has some unknown comment
+    # which we will ignore.
+    """
+    ls /
+    #1571012545
+    echo foo
+    #1571012560
+    #useless comment that should be ignored.
+    cat bar
+    """
+    history = []
+    # Phase 1 starts: After this phase history will be like this
+    # [(-1, "ls /"), # This entry has no timestamp.
+    #  (1571012545, "echo foo"),
+    #  (1571012560, "cat bar")]
+    last_ts = -1
+    for line in open(os.path.expanduser("~/.bash_history")):
+        if line[0] == '#':
+            try:
+                last_ts = int(line[1:].strip())
+            except Exception:
+                # Ignore the exception.
+                pass
+            continue
+        history.append([last_ts, line.strip()])
+
+    # Phase 2 starts: After this phase history will be like this
+    # [(1571012545, "ls /"), # Timestamp for this comes from its next entry
+    #  (1571012545, "echo foo"),
+    #  (1571012560, "cat bar")]
+    last_ts = -1
+    for i in range(len(history) - 1, -1, -1):
+        if history[i][0] == -1 and last_ts != -1:
+            history[i][0] = last_ts
+        elif history[i][0] != -1 and last_ts == -1:
+            last_ts = history[i][0]
+    # Add the history entries into recent's DB.
+    conn = create_connection()
+    import random
+    # Create a session with a random -ve pid and randome -ve sequence id.
+    pid = -random.randint(1, 10000000)
+    session = Session(pid=pid, sequence=-random.randint(1, 10000000))
+    session.update(conn)
+    for cmd_ts, cmd in history:
+        c = conn.cursor()
+        c.execute(SQL.INSERT_ROW_CUSTOM_TS, [
+            cmd_ts, cmd, pid,
+            # exit status=-1, working directory=/unknown
+            -1, "/unknown",
+            session.id])
     conn.commit()
     conn.close()
 
@@ -292,6 +363,7 @@ def check_prompt():
         exit(1)
 
 
+# Entry point to recent command.
 def main():
     check_prompt()  # Fail the command if PROMPT_COMMAND is not set
     parser = make_arg_parser_for_recent()
@@ -312,6 +384,7 @@ def main():
 
 
 if __name__ == '__main__':
+    import_bash_history()
     print("=================")
     print("Executing recent from __main__.")
     print("This means recent2 is being run via `python recent2.py`")
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ LONG_DESCRIPTION = open('README.md').read()
 
 setup(
     name='recent2',
-    version='0.2.1',
+    version='0.3',
     description='Logs bash history to an sqlite database',
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
@@ -33,6 +33,7 @@ setup(
     entry_points={
         'console_scripts': [
             'log-recent=recent2:log',
+            'recent-import-bash-history=recent2:import_bash_history',
             'recent=recent2:main',
         ],
     },
