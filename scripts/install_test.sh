38ae4f343b5473eb428ff5e9f90154048dc19de1
Sai Teja Suram <pratap130492@gmail.com>
2020-02-05 15:26:04 -0800

    more

--- scripts/install_test.sh
+++ scripts/install_test.sh
@@ -1,4 +1,10 @@
 set -o xtrace
+# Install recent
 pip install -e .
+# update prompt command.
 export PROMPT_COMMAND='log-recent -r $? -c "$(HISTTIMEFORMAT= history 1)" -p $$'
+# Run some commands.
+echo "wow"
+date
+# check if recent shows these.
 recent
