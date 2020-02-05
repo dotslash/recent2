228eea4d24bff0bc5df81005aa0e514687e029c0
Sai Teja Suram <pratap130492@gmail.com>
2020-02-05 15:40:23 -0800

    hmm

--- scripts/install_test.sh
+++ scripts/install_test.sh
@@ -11,6 +11,6 @@ export PROMPT_COMMAND='log-recent -r $? -c "$(HISTTIMEFORMAT= history 1)" -p $$'
 echo "wow"
 date
 # check if recent shows these.
-recent-import-bash-history
+history
 recent
 recent
