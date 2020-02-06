b79c15904de22fac170d21f9103b1468f28dd3ae
Sai Teja Suram <pratap130492@gmail.com>
2020-02-05 16:20:00 -0800

    hmm

--- scripts/install_test.sh
+++ scripts/install_test.sh
@@ -7,8 +7,8 @@ code_dir=$(pwd)
 echo $code_dir
 cd $HOME
 pwd
-pip  --yes uninstall recent2 || true
-pip  install -e $code_dir
+pip uninstall --yes recent2 || true
+pip install -e $code_dir
 # update prompt command. Otherwise recent command will fail.
 export PROMPT_COMMAND='log-recent -r $? -c "$(HISTTIMEFORMAT= history 1)" -p $$'
 recent # Run the recent command just so that we know it kind of works.
