2c5aed5051169528c0cba5bb7dfaec74dd7f38c6
Sai Teja Suram <pratap130492@gmail.com>
2020-02-05 16:01:07 -0800

    mroe

--- scripts/install_test.sh
+++ scripts/install_test.sh
@@ -1,16 +1,13 @@
-set -o xtrace
-# Install recent from a different directory
+set -o xtrace # Print the commands we are running
+# Install `recent` from a different directory. By running from different
+# dir, we will verify that we are not relying on files in the directory
+# and not files we export in the python package.
+# TODO(dotslash): May be upload to test pypi and install it.
 code_dir=$(pwd)
 echo $code_dir
 cd $HOME
 pwd
 pip install -e $code_dir
-# update prompt command.
+# update prompt command. Otherwise recent command will fail.
 export PROMPT_COMMAND='log-recent -r $? -c "$(HISTTIMEFORMAT= history 1)" -p $$'
-# Run some commands.
-echo "wow"
-date
-# check if recent shows these.
-history
-recent
-recent
+recent # Run the recent command just so that we know it kind of works.
