3cb76eff96584957b45423d9cb25f98a1bfc2a89
Sai Teja Suram <pratap130492@gmail.com>
2020-02-23 20:42:25 -0800

    this will work :)

--- scripts/install_test.sh
+++ scripts/install_test.sh
@@ -3,19 +3,32 @@ set -o xtrace
 # exit when any command fails
 set -e
 
+
 # Ensure that recent2 is not installed.
 pip uninstall --yes recent2 || true
 python setup.py bdist_wheel
 pip install dist/recent2-*-py3-none-any.whl
+
+### LET'S DO SOME VERY BASIC TESTING ###
+
 # update prompt command. Otherwise recent command will fail.
 export PROMPT_COMMAND='log-recent -r $? -c "$(HISTTIMEFORMAT= history 1)" -p $$'
-# Explicitly log some command with pid 4556 and return value 0
+
+# Initialize the session. "command from different session" will not 
+# be recorded.
+log-recent -r 0 -c " 122 command from different session" -p 4556
+# Explicitly log "some command" with pid 4556 and return value 0
 log-recent -r 0 -c " 123 some command" -p 4556
-# Explicitly log some other with pid 4556 and return value 0
+# Explicitly log "some other command" with pid 4556 and return value 1
 log-recent -r 1 -c " 124 some other command" -p 3455
+
 # Run the recent command just so that we know it kind of works.
 recent > /tmp/out
 # Grep for the commands. Grep returns failure if it does not find the text.
-cat /tmp/out
+cat /tmp/out # For debugging.
 grep "some command" /tmp/out
 grep "some other command" /tmp/out
+# Check thet the tmp file does not contain the unexpected command
+OUT="does_not_contain"
+(grep "command from different session" /tmp/out && OUT="contains") || true
+[[ $OUT=="does_not_contain" ]]
