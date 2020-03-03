5ddec41da6da673a897dedf7ae896d0116028b16
Sai Teja Suram <pratap130492@gmail.com>
2020-03-03 00:46:10 -0800

    Support querying env variables

--- recent2.py
+++ recent2.py
@@ -38,8 +38,11 @@ class SQL:
         values (datetime('now','localtime'), datetime('now','localtime'), ?, ?, ?, ?, ?)"""
     UPDATE_SESSION = """update sessions set updated_dt = datetime('now','localtime'), sequence = ?
         where session = ?"""
-    TAIL_N_ROWS = """select command_dt, command from (select * from commands where
-        order by command_dt desc limit ?) order by command_dt"""
+    # TAIL_N_ROWS's columns (column order is same as TAIL_N_ROWS
+    COLUMNS = 'command_dt,command,pid,return_val,pwd,session,json_data'.split(',')
+    TAIL_N_ROWS = """select command_dt, command, pid, return_val, pwd, session, json_data
+                     from (select * from commands where
+                     order by command_dt desc limit ?) order by command_dt"""
     GET_SESSION_SEQUENCE = """select sequence from sessions where session = ?"""
 
     # Setup: Create tables.
@@ -327,6 +330,13 @@ def query_builder(args, parser):
     if args.d:
         filters.append(parse_date(args.d))
         parameters.append(args.d)
+    for env_var in args.env:
+        split = env_var.split(":")
+        if len(split) == 1:
+            filters.append('json_extract(json_data, "$.env.{}") is not null'.format(split[0]))
+        else:
+            filters.append('json_extract(json_data, "$.env.{}") = ?'.format(split[0]))
+            parameters.append(split[1])
     filters.append('length(command) < {}'.format(args.char_limit))
     try:
         n = int(args.n)
@@ -388,12 +398,26 @@ def make_arg_parser_for_recent():
         metavar='200',
         help='Ignore commands longer than this.',
         default=200)
-    parser.add_argument('--debug', help='Debug mode', action='store_true')
+    parser.add_argument(
+        '-e', '--env',
+        action='append',
+        help='Filter by shell env vars',
+        metavar='key[:val]',
+        default=[])
 
+    # CONTROL OUTPUT FORMAT
     # Hide time. This makes copy-pasting simpler.
     parser.add_argument(
         '--hide_time', '-ht',
         help='dont display time in command output', action='store_true')
+    parser.add_argument('--debug', help='Debug mode', action='store_true')
+    parser.add_argument('--detail', help='Return detailed output', action='store_true')
+    parser.add_argument(
+        '--columns',
+        help=('Comma separated columns to print if --detail is passed. Valid columns are '
+              'command_dt,command,pid,return_val,pwd,session,json_data'),
+        default="command_dt,command,json_data")
+
     # Query type - regex/sql.
     parser.add_argument(
         '-re', help='enable regex search pattern', action='store_true')
@@ -440,14 +464,32 @@ def main():
 
     conn.set_trace_callback(update_queries_executed)
     c = conn.cursor()
+    detail_results = []
+    columns_to_print = args.columns.split(',')
     for query, parameters in query_builder(args, parser):
         for row in c.execute(query, parameters):
-            if not (row[0] and row[1]):
+            row_dict = {SQL.COLUMNS[i]: row[i]
+                        for i in range(len(row))
+                        if SQL.COLUMNS[i] in columns_to_print}
+            if not (row_dict['command_dt'] and row_dict['command']):
+                continue
+            if args.detail:
+                detail_results.append(row_dict)
                 continue
             if args.hide_time:
-                print(row[1])
+                print(row_dict['command'])
             if not args.hide_time:
-                print(Term.WARNING + row[0] + Term.ENDC + ' ' + row[1])
+                print(Term.WARNING + row_dict['command_dt'] + Term.ENDC + ' ' + row_dict['command'])
+    if args.detail:
+        if 'json_data' not in columns_to_print:
+            from tabulate import tabulate
+            print(tabulate(detail_results, headers="keys"))
+        else:
+            for res in detail_results:
+                for k, v in res.items():
+                    print(Term.BOLD + Term.OKBLUE + k + Term.ENDC + ": " + str(v))
+                print("---------------------------------")
+
     if args.debug:
         schema = None
         for row in c.execute(SQL.GET_COMMANDS_TABLE_SCHEMA, []):
